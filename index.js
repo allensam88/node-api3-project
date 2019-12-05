86ab6bca95a0958bacf456356ee91f90ba857a96
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-12-05 08:34:14 -0800

    added some comments for step-by-step set-up instructions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// 1.
 const server = require("./server.js");
-
+// 2.
 server.listen(4000, () => {
     console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const userRouter = require('./users/userRouter.js');
-const postRouter = require('./posts/postRouter.js');
-const server = express();
+const express = require('express');  // <<<<< 1.
+const userRouter = require('./users/userRouter.js');  // <<<<< 2.
+const postRouter = require('./posts/postRouter.js');  
 
-server.use(express.json());
-server.use(logger);
+const server = express(); // <<<<< 3.
 
-server.get('/', (req, res) => {
-    res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
-server.use('/api/users', userRouter);
+server.use(express.json());  // <<<<< 4.
+server.use('/api/users', userRouter);  // <<<<< 5.
 server.use('/api/posts', postRouter);
 
-module.exports = server;
+server.use(logger);  // <<<<< 9. (skip if you don't have any global middleware)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);  // <<<<< 6.
+});
+
+
+module.exports = server;  // <<<<< 7.
 
 //*****CUSTOM MIDDLEWARE*****\\
 // 1. logger: logs to the console the following information about each request: request method, request url, and a timestamp
 // this middleware runs on every request made to the API
 function logger(req, res, next) {
-    console.log(`${req.method} to url:${req.originalUrl} at ${Date()}`)
+    console.log(`${req.method} to url:${req.originalUrl} at ${Date()}`)  // <<<<< 8.
     next();
 }
